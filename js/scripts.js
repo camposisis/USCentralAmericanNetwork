a41f37c22583e875b98bce93ff538fa9c876ea93
Isis Campos <39924708+camposisis@users.noreply.github.com>
2021-05-23 00:57:57 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -55,5 +55,18 @@ window.addEventListener('DOMContentLoaded', event => {
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+ 
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        pagination:false,
+        navigationText:false
+    });
+});
 
 });
